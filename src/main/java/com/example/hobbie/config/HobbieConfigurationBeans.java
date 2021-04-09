7ff8a63cd2ac02f408462377a8fbbaf0c3c2198f
purshink <nikoletadoykova@gmail.com>
2021-04-09 11:31:48 +0200

    Abo service testing

--- src/main/java/com/example/hobbie/config/HobbieConfigurationBeans.java
+++ src/main/java/com/example/hobbie/config/HobbieConfigurationBeans.java
@@ -3,6 +3,7 @@ package com.example.hobbie.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.core.session.SessionInformation;
--- src/test/java/com/example/hobbie/service/impl/AboServiceImplTest.java
+++ src/test/java/com/example/hobbie/service/impl/AboServiceImplTest.java
@@ -3,9 +3,12 @@ package com.example.hobbie.service.impl;
 import com.example.hobbie.handler.FailToDeleteException;
 import com.example.hobbie.handler.NotFoundException;
 import com.example.hobbie.model.entities.Abo;
+import com.example.hobbie.model.entities.AppClient;
+import com.example.hobbie.model.entities.BusinessOwner;
 import com.example.hobbie.model.repostiory.AboRepository;
 import com.example.hobbie.service.AboService;
 import com.example.hobbie.service.UserService;
+import com.example.hobbie.view.AboViewModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,11 +17,12 @@ import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssumptions.given;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyVararg;
+import static org.mockito.Mockito.*;
 
 class AboServiceImplTest {
 
@@ -46,7 +50,6 @@ class AboServiceImplTest {
     }
 
 
-
     @Test
     void getUserAbos_should_work() {
         List<Abo> byId = new ArrayList<>();
@@ -61,9 +64,9 @@ class AboServiceImplTest {
         Assertions.assertThrows(
                 NotFoundException.class, () -> {
                     aboServiceTest.findAboById(null);
-                   aboServiceTest.findAboId(null);
-                   aboServiceTest.getClientDetails(null);
-                   aboServiceTest.deleteAbo(null);
+                    aboServiceTest.findAboId(null);
+                    aboServiceTest.getClientDetails(null);
+                    aboServiceTest.deleteAbo(null);
                 }
         );
     }
@@ -82,37 +85,137 @@ class AboServiceImplTest {
         );
     }
 
-//    @Test
-//    void findAboId() {
-//    }
+    @Test
+    void findAboById_should_work() {
+        when(mockAboRepository.findById(1L)).thenReturn(Optional.of(abo));
+        AboViewModel aboViewModel = this.modelMapper.map(abo, AboViewModel.class);
+        AboViewModel aboById = aboServiceTest.findAboById(1L);
+        assertEquals(aboById.getId(), aboViewModel.getId());
+    }
 
-//    @Test
-//    void updateAbosWithEntries() {
-//
-//    }
-//
-//
-//    @Test
-//    void getAbosPerBusiness() {
-//    }
-//
-//    @Test
-//    void getClientDetails() {
-//    }
-//
-//    @Test
-//    void findAboById() {
-//    }
-//
-//    @Test
-//    void getExcistingAbosForClient() {
-//    }
-//
-//    @Test
-//    void deleteAbo() {
-//    }
-//
-//    @Test
-//    void findExcistingAbosWithHobbyId() {
-//    }
+    @Test
+    void findAboById_should_throw() {
+        Assertions.assertThrows(
+                NotFoundException.class, () -> {
+                    aboServiceTest.findAboById(null);
+
+                }
+        );
+
+    }
+
+    @Test
+    void findAboId_should_work() {
+        when(mockAboRepository.findByEntriesId(1L)).thenReturn(Optional.of(abo));
+        Long aboId = aboServiceTest.findAboId(1L);
+        assertEquals(aboId, abo.getId());
+    }
+
+    @Test
+    void findAboId_should_throw() {
+        Assertions.assertThrows(
+                NotFoundException.class, () -> {
+                    aboServiceTest.findAboId(null);
+
+                }
+        );
+    }
+
+    @Test
+    void deleteAbo_should_throw() {
+        Assertions.assertThrows(
+                NotFoundException.class, () -> {
+                    aboServiceTest.deleteAbo(null);
+
+                }
+        );
+    }
+
+    @Test
+    void deleteAbo_should_work() {
+        when(mockAboRepository.findById(1L))
+                .thenReturn(Optional.of(abo));
+
+        aboServiceTest.deleteAbo(1L);
+
+        Mockito.verify(mockAboRepository, times(1)).delete(abo);
+    }
+
+    @Test
+    void findExcistingAbosWithHobbyId_should_work() {
+        aboServiceTest.findExcistingAbosWithHobbyId(1L);
+        Mockito.verify(mockAboRepository, times(1)).findByHobbyId(1L);
+    }
+
+
+    @Test
+    void findAbo_should_work() {
+        when(mockAboRepository.findById(1L))
+                .thenReturn(Optional.of(abo));
+
+        Abo abo1 = aboServiceTest.findAbo(1L);
+        assertEquals(abo.getId(), abo1.getId());
+    }
+
+    @Test
+    void findAbo_should_throw() {
+        Assertions.assertThrows(
+                NotFoundException.class, () -> {
+                    aboServiceTest.findAbo(null);
+
+                }
+        );
+    }
+
+    @Test
+    void getExcistingAbosForClient() {
+        List<Abo> abos = new ArrayList<>();
+        abos.add(abo);
+        when(mockAboRepository.findByClientId(1L))
+                .thenReturn(abos);
+        aboServiceTest.getExcistingAbosForClient(1L);
+        assertEquals(1, aboServiceTest.getExcistingAbosForClient(1L).size());
+
+    }
+
+    @Test
+    void getAbosPerBusiness() {
+        BusinessOwner bs = new BusinessOwner();
+        bs.setId(1L);
+        when(userService.findCurrentUserBusinessOwner())
+                .thenReturn(bs);
+
+        List<Abo> abos = new ArrayList<>();
+        abos.add(abo);
+        when(mockAboRepository.findAllByBusinessOwnerId(1L))
+                .thenReturn(abos);
+        assertEquals(1, aboServiceTest.getAbosPerBusiness().size());
+    }
+
+    @Test
+    void getClientDetails() {
+        when(mockAboRepository.findById(1L))
+                .thenReturn(Optional.of(abo));
+        abo.setClientId(2L);
+        AppClient appClient = new AppClient();
+        appClient.setId(2L);
+        when(userService.findAppClientById(2L))
+                .thenReturn(appClient);
+        assertEquals(abo.getClientId(), aboServiceTest.getClientDetails(1L).getId());
+
+
+    }
+    @Test
+    void updateAbosWithEntries() {
+        List<Abo> abos = new ArrayList<>();
+        abos.add(abo);
+
+        aboServiceTest.updateAbosWithEntries(abos);
+        Mockito.verify(mockAboRepository, times(1)).saveAll(abos);
+
+    }
 }
+
+
+
+
